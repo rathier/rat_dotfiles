88b8ce22ff15cf4dc8e137f0339aeafcd174f87b
Rainer Thierfelder <rainer.thierfelder+github@googlemail.com>
2020-06-07 17:04:50 +0200

    makelinks: shellcheck fixes

--- makelinks.bash
+++ makelinks.bash
@@ -17,23 +17,28 @@ setup_vundle() {
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "...done"
 
+# testing, if $dir exists
+if [ ! -d "$dir" ] ; then
+    echo "$dir not found"
+    exit 1
+fi
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd "$dir"
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     # -f tests for regular file, -h test for symbolic link
-    if [ -f ~/.$file -a ! -h ~/.$file ]; then
+    if [ -f ~/."$file" ] && [ ! -h ~/."$file" ]; then
         echo "Move .$file from ~ to $olddir"
-        mv ~/.$file ~/dotfiles_old/
+        mv ~/."$file" ~/dotfiles_old/
     fi
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s "$dir"/"$file" ~/."$file"
 done
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
